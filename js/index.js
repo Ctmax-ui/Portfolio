ded2be2f6f8fc1947643778d0606d2e2ef2ae6bb
Debjeet <debjeetbiswas121@gmail.com>
2024-03-31 09:03:20 +0530

    small changes

--- js/index.js
+++ js/index.js
@@ -66,13 +66,3 @@ animateCircles();
 
 
 // for box light eff
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
